e99113f84930c9f851d7cdd19dd63da621745952
shmudivel <82043832+shmudivel@users.noreply.github.com>
2023-06-27 15:58:26 -0400

    skeleton home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="flex-start flex-col paddings mb-16 ">
+      <h1>Categorires</h1>
+      <h1>Post</h1>
+      <h1>Load more</h1>
+    </section>
   );
 };
 
